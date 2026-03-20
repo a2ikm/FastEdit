ce35c71aff37f7912391dafbe7716185afe42e72
Masato Ikeda <masato.ikeda@gmail.com>
2026-03-20 23:42:44 +0900

    Add line wrap toggle to View menu
    Toggle between wrapped and unwrapped text via View > Wrap/Unwrap Lines
    (⌥⌘L). Unwrapped mode enables horizontal scrolling.

--- FastEdit/ViewController.swift
+++ FastEdit/ViewController.swift
@@ -4,6 +4,8 @@ class ViewController: NSViewController {
 
     @IBOutlet var textView: NSTextView!
 
+    private var isLineWrapping: Bool = true
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTextView()
@@ -24,6 +26,50 @@ class ViewController: NSViewController {
         textView.isAutomaticTextReplacementEnabled = false
         textView.isAutomaticSpellingCorrectionEnabled = false
         textView.usesFindBar = true
+    }
+
+    // MARK: - Line Wrapping
+
+    @IBAction func toggleLineWrapping(_ sender: Any?) {
+        isLineWrapping.toggle()
+        applyLineWrapping()
+    }
+
+    private func applyLineWrapping() {
+        guard let scrollView = textView.enclosingScrollView,
+              let textContainer = textView.textContainer else { return }
+
+        if isLineWrapping {
+            textContainer.widthTracksTextView = true
+            textContainer.containerSize = NSSize(
+                width: scrollView.contentSize.width,
+                height: CGFloat.greatestFiniteMagnitude
+            )
+            textView.isHorizontallyResizable = false
+            scrollView.hasHorizontalScroller = false
+        } else {
+            textContainer.widthTracksTextView = false
+            textContainer.containerSize = NSSize(
+                width: CGFloat.greatestFiniteMagnitude,
+                height: CGFloat.greatestFiniteMagnitude
+            )
+            textView.isHorizontallyResizable = true
+            scrollView.hasHorizontalScroller = true
+        }
+
+        textView.needsDisplay = true
+        textView.invalidateIntrinsicContentSize()
+    }
+
+}
+
+extension ViewController: NSMenuItemValidation {
+    func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
+        if menuItem.action == #selector(toggleLineWrapping(_:)) {
+            menuItem.title = isLineWrapping ? "Unwrap Lines" : "Wrap Lines"
+            return true
+        }
+        return true
     }
 }
 

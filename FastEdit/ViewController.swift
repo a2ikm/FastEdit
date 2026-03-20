1a736e7c932e83894d784d718ed46a5d4ccfb998
Masato Ikeda <masato.ikeda@gmail.com>
2026-03-20 23:18:39 +0900

    Initial Commit

---
+++ FastEdit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FastEdit
+//
+//  Created by Masato Ikeda on 2026/03/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
